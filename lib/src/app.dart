10068a00468c2b8e2694881a2c766d8141ddcb5c
Alex Zafra <azafrom731@g.educaand.es>
2021-09-30 14:42:28 +0200

    AppBar

---
+++ lib/src/app.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:primera_aplicacion/src/pages/home_page.dart';
+
+class MyApp extends StatelessWidget{
+
+  @override
+  Widget build(BuildContext context) {
+    
+    // ignore: prefer_const_constructors
+    return MaterialApp(
+      // ignore: prefer_const_constructors
+      home: HomePage()
+      
+    );
+  }
+}
